8784b98e7fd338ee65f5fff2352c6739629567d0
Aparallel <aparallel0718@gmail.com>
2026-08-31 01:21:03 +0530

    replace sidebar with top nav

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { authOptions } from '../../lib/auth';
-import Sidebar from '../../components/Sidebar';
+import TopNav from '../../components/TopNav';
 
 export default async function DashboardLayout({ children }) {
   const session = await getServerSession(authOptions);
@@ -9,7 +9,7 @@ export default async function DashboardLayout({ children }) {
 
   return (
     <div className="flex min-h-screen">
-      <Sidebar role={session.user.role} name={session.user.name} />
+      <TopNav role={session.user.role} name={session.user.name} />
       <main className="flex-1 p-6 md:p-8 bg-chalk">{children}</main>
     </div>
   );
